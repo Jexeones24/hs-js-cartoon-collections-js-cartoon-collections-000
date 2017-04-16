488e99d06ca0bc900de301acc3721553f88b8e62
Jessica Pamanian <jexeones@gmail.com>
2017-04-16 18:12:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 function dwarfRollCall(dwarves) {
+  var numberedDwarves = [];
+  for (var i = 0; i < dwarves.length; i++) {
+    numberedDwarves.push(`${i + 1}. ${dwarves[i]}`);
+  }
+    return `${numberedDwarves.join(' ')} `;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var planeteerShout = [];
+  for (var i = 0; i < planeteerCalls.length; i++) {
+    planeteerShout.push(planeteerCalls[i].toUpperCase + '!');
+  }
+  return planeteerShout;
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i++) {
+    return words[i].length > 4 ?
+      1 === 1 : 0 === 1
+  }
 }
 
-function findTheCheese (foods) {
+
+var cheese = ['camembert', 'cheddar', 'gouda'];
+
+function findTheCheese(foods) {
+  for (var i = 0; i < foods.length; i++) {
+    for (var j = 0; j < cheese.length; j++) {
+      if (foods[i] == cheese[j]) {
+        return foods[i];
+      }
+    }
+  }
+  return "no cheese!"
 }
